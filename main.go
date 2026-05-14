ede1c388295088f7ec87361f66d72eab7dc3577a
Danila Solovenko <dansil2301@gmail.com>
2026-05-15 00:24:37 +0200

    feat: route add

--- main.go
+++ main.go
@@ -1,13 +1,17 @@
 package main
 
-import "github.com/gin-gonic/gin"
+import (
+	"log"
+	"secret/routes"
+
+	"github.com/gin-gonic/gin"
+)
 
 func main() {
 	router := gin.Default()
-	router.GET("/ping", func(c *gin.Context) {
-		c.JSON(200, gin.H{
-			"message": "pong",
-		})
-	})
+
+	routes.RegisterRoutes(router)
+
+	log.Printf("Server starting")
 	router.Run(":8080")
 }
---
+++ routes/routes.go
@@ -0,0 +1,15 @@
+package routes
+
+import "github.com/gin-gonic/gin"
+
+func RegisterRoutes(router *gin.Engine) {
+	// Public
+	public := router.Group("/api")
+	{
+		public.GET("/ping", func(c *gin.Context) {
+			c.JSON(200, gin.H{
+				"message": "pong",
+			})
+		})
+	}
+}
