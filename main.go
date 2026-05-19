3cd21dfabf8dd14956cd57aa16cb5943659919a2
Danila Solovenko <dansil2301@gmail.com>
2026-05-19 19:35:20 +0200

    feat: add base config

--- main.go
+++ main.go
@@ -2,16 +2,20 @@ package main
 
 import (
 	"log"
+	"secret/config"
 	"secret/routes"
 
 	"github.com/gin-gonic/gin"
 )
 
 func main() {
+	gin.SetMode(config.GetConfig().ServerMode)
+
 	router := gin.Default()
 
 	routes.RegisterRoutes(router)
 
-	log.Printf("Server starting")
-	router.Run(":8080")
+	port := config.GetConfig().ServerPort
+	log.Printf("Server starting on port %s", port)
+	router.Run(":" + port)
 }
